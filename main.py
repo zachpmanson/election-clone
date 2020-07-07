7e9eefe70aeb4dbe2b77b79b8000a469c9691424
callmepavo <z1verdeman2@gmail.com>
2020-07-07 09:54:42 +0800

    added gitignore

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 if __name__ == "__main__":
     from election import *
 
+    print("Election 0")
     e0 = Election("election0.txt")
     e0.process_file()
     e0.conduct_count()
     e0.get_standings()
 
+    print("Election 1")
     e1 = Election("election1.txt")
     e1.process_file()
     e1.conduct_count()
